545d18685da3d73109ce7bcdb14a8b3ad4ef59ef
balajiraj7863 <30717321+balajiraj7863@users.noreply.github.com>
2018-05-28 18:10:43 +0530

    build script 2

--- build.py
+++ build.py
@@ -6,13 +6,13 @@ import shutil
 def replace_string(**kwargs):
     path_name=kwargs['pathVar']
     drc = path_name
-    backup = path_name+'/tmp'
+    #backup = path_name+'/tmp'
     pattern = re.compile('hi')
     oldstr = 'hi'
     newstr = 'Python'
 
     print (path_name)
-    
+
     for dirpath, dirname, filename in os.walk(drc):
         #Getting a list of the full paths of files
         for fname in filename:
@@ -20,7 +20,7 @@ def replace_string(**kwargs):
             strg = open(path).read() #Opening the files for reading only
             if re.search(pattern, strg):#If we find the pattern ....
                 #print path, strg
-                shutil.copy2(path, backup) #we will create a backup of it
+                #shutil.copy2(path, backup) #we will create a backup of it
                 strg = strg.replace(oldstr, newstr) #We will create the replacement condistion
                 f = open(path, 'w') #We open the files with the WRITE option
                 f.write(strg) # We are writing the the changes to the files
