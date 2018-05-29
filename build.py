4f8ac4aed64254ab4ebee5f17ff6f0992a71b816
balajiraj7863 <balaji.rajendran@prodapt.com>
2018-05-29 13:41:40 +0530

    bs5

--- build.py
+++ build.py
@@ -30,7 +30,7 @@ def replace_string(**kwargs):
                 f.write(strg) # We are writing the the changes to the files
                 f.close() #Closing the files
     # archiev the files
-    shutil.make_archive("name","zip", zip_path) ## archiev the files zip
+    shutil.make_archive("name","zip", zip_path+'/codeNew') ## archiev the files zip
     # archiev the files final
 
 
