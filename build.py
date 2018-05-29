e8f0eb47c4f130510cca6ada30567de22dc6a5e8
balajiraj7863 <balaji.rajendran@prodapt.com>
2018-05-29 14:49:37 +0530

    bs7

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ from shutil import make_archive
 def replace_string(**kwargs):
     path_name=kwargs['pathVar']
     zip_path=kwargs['zipPathVar']
-    newstr=kwargs['newString']
+    newappName=kwargs['appName']
     #version_name=kwargs['newVersion']
     drc = path_name
     #backup = path_name+'/tmp'
@@ -26,7 +26,7 @@ def replace_string(**kwargs):
             if re.search(pattern, strg):#If we find the pattern ....
                 #print path, strg
                 #shutil.copy2(path, backup) #we will create a backup of it
-                strg = strg.replace(oldstr, newstr) #We will create the replacement condistion
+                strg = strg.replace(oldstr, newappName) #We will create the replacement condistion
                 f = open(path, 'w') #We open the files with the WRITE option
                 f.write(strg) # We are writing the the changes to the files
                 f.close() #Closing the files
@@ -40,12 +40,12 @@ if __name__=="__main__":
     #parser.add_argument('--version',help="Release Version you are going to deploy")
     parser.add_argument('--pathVar',help="path to start change",required=True)
     parser.add_argument('--zipPathVar',help="path to start change",required=True)
-    parser.add_argument('--newString',help="path to start change",required=True)
+    parser.add_argument('--appName',help="path to start change",required=True)
 
     vars=parser.parse_args()
 
     pathValue=vars.pathVar.strip()
     zipPathValue=vars.zipPathVar.strip()
-    newString=vars.newString.strip()
+    appName=vars.appName.strip()
 
-    replace_string(pathVar=pathValue, zipPathVar=zipPathValue, newstr=newString)
+    replace_string(pathVar=pathValue, zipPathVar=zipPathValue, newappName=appName)
