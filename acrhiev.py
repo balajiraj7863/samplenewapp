a0ed7322ab00bea5c9b6c75eed7010172c8a2596
balajiraj7863 <balaji.rajendran@prodapt.com>
2018-05-30 13:13:23 +0530

    dd

---
+++ acrhiev.py
@@ -0,0 +1,9 @@
+import re
+import argparse
+import sys, os
+import shutil
+from zipfile import ZipFile
+from shutil import make_archive
+import configparser
+
+shutil.make_archive('D:\GIT\sampleapp',"zip", 'D:\GIT\sampleapp/app')
