55a18f88af99523cfd375e3456199ab175165e20
balajiraj7863 <balaji.rajendran@prodapt.com>
2018-05-31 17:24:17 +0530

    bb11

--- build.py
+++ build.py
@@ -39,9 +39,9 @@ def replace_string(**kwargs):
                 #print (strg)
                 f.close() #Closing the files
     # archiev the files
-    # if not os.path.exists(zip_path+"/"+newapp_name):
-    #     os.makedirs(zip_path+"/"+newapp_name)
     shutil.make_archive(zip_path+"/"+newapp_name,"zip", path_name) ## archiev the files zip
+    if not os.path.exists(zip_path+"/"+newapp_name):
+        os.makedirs(zip_path+"/"+newapp_name)
     # archiev the files final
 
 
